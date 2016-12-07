d60843f10049e4064559fc94692431feea239f7a
Millie Walsh <millie@primeacademy.io>
2016-12-07 11:50:25 -0600

    add body parser

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 // make these file publicly available
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -12,6 +15,7 @@ app.get('/', function(req, res) {
 
 app.post('/bob', function(req, res) {
   console.log('inside the post route');
+  console.log('req.body', req.body);
   res.sendStatus(200); // OK
 });
 
